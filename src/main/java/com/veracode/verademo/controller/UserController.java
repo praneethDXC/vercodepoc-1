0675c7c283d689b8c854a1f4f8329b2e4a17b4ab
praneeeth18 <praneeth608@gmail.com>
2024-12-05 16:10:19 +0530

    XSS Java UserController remediated latest

--- src/main/java/com/veracode/verademo/controller/UserController.java
+++ src/main/java/com/veracode/verademo/controller/UserController.java
@@ -19,7 +19,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -46,6 +45,7 @@ import com.veracode.verademo.utils.Utils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.owasp.encoder.Encode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
@@ -231,40 +231,40 @@ public class UserController {
 	@RequestMapping(value = "/password-hint", method = RequestMethod.GET)
 	@ResponseBody
 	public String showPasswordHint(String username) {
-		logger.info("Entering password-hint with username: " + username);
+	    logger.info("Entering password-hint with username: " + Encode.forJava(username));
 
-		if (username == null || username.isEmpty()) {
-			return "No username provided, please type in your username first";
-		}
+	    if (username == null || username.isEmpty()) {
+	        return Encode.forHtml("No username provided, please type in your username first");
+	    }
 
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
+	    try {
+	        Class.forName("com.mysql.jdbc.Driver");
 
-			Connection connect = DriverManager.getConnection(Constants.create().getJdbcConnectionString());
+	        Connection connect = DriverManager.getConnection(Constants.create().getJdbcConnectionString());
 
-			String sql = "SELECT password_hint FROM users WHERE username = ?";
-			logger.info(sql);
-			PreparedStatement statement = connect.prepareStatement(sql);
-			statement.setString(1, username);
-			ResultSet result = statement.executeQuery();
-			if (result.first()) {
-				String password = result.getString("password_hint");
-				String formatString = "Username '" + username + "' has password: %.2s%s";
-				logger.info(formatString);
-				return String.format(
-						formatString,
-						password,
-						String.format("%0" + (password.length() - 2) + "d", 0).replace("0", "*"));
-			} else {
-				return "No password found for " + username;
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	        String sql = "SELECT password_hint FROM users WHERE username = ?";
+	        logger.info(sql);
+	        PreparedStatement statement = connect.prepareStatement(sql);
+	        statement.setString(1, username);
+	        ResultSet result = statement.executeQuery();
+	        if (result.first()) {
+	            String password = result.getString("password_hint");
+	            String formatString = "Username '" + Encode.forHtml(username) + "' has password: %.2s%s";
+	            logger.info(formatString);
+	            return Encode.forHtml(String.format(
+	                    formatString,
+	                    password,
+	                    String.format("%0" + (password.length() - 2) + "d", 0).replace("0", "*")));
+	        } else {
+	            return Encode.forHtml("No password found for " + username);
+	        }
+	    } catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
 
-		return "ERROR!";
+	    return Encode.forHtml("ERROR!");
 	}
 
 	@RequestMapping(value = "/logout", method = { RequestMethod.GET, RequestMethod.POST })
